c63f98b1da594885065058ead34ef22658cf258c
aquispesegales <alvaroquispesegales@gmail.com>
2024-02-03 18:32:30 -0400

    aaaa

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import * as fs  from 'fs';
 
 async function bootstrap() {
 
-  /*const httpsOptions = {
-    key: fs.readFileSync('I:/key/key.pem'),
-    cert: fs.readFileSync('I:/key/cert.pem'),
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/key.pem'),
+    cert: fs.readFileSync('./secrets/cert.pem'),
   };
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
-  });*/
+  });
 
-  const app = await NestFactory.create(AppModule);
+  //const app = await NestFactory.create(AppModule);
 
   await app.listen(9082);
 }
